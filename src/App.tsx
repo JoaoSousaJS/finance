1a64c9fcd38e5ea6fb1c306cbadf04f1668a6c23
Joao Sousa <joao.sousa@sine.co>
2023-03-30 08:36:14 +1030

    feat: add theme for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useMemo } from "react";
 import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
