5261f6f0fa1988d65eccb3809e3d41e23f33cd8d
Joao Sousa <joao.sousa@sine.co>
2023-03-31 09:25:29 +1030

    feat: add initial dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { themeSettings } from "./theme";
 import { Box, CssBaseline } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./scenes/navbar";
+import Dashboard from "./scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -16,7 +17,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <NavBar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route
                 path="/predictions"
                 element={<div>predictions page</div>}
