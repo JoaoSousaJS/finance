e312cc2e4ce0d78b69ac63f703f9bb1eecbd10ed
Joao Sousa <joao.rsousa23@gmail.com>
2023-04-03 17:30:57 +0930

    feat: add template area

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import {
+  gridTemplateLargeScreens,
+  gridTemplateSmallScreens,
+} from "./templateConstant";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
-  return <div>index</div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const { palette } = useTheme();
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
